c457fa3e6a742f736027ce9b8024d6db7a60bd8f
Casey Patrick Driscoll <caseypatrickdriscoll@me.com>
2015-01-10 16:05:01 -0500

    Init commit

---
+++ hookbook.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Plugin Name: HookBook
+ * Plugin URI: http://patchedupcreative.com/hookbook
+ * Version: 1.0
+ * Author: Casey Patrick Driscoll
+ * Author URI: http://caseypatrickdriscoll.com
+ * Description: A plugin for finding hooks in WordPress and generating docs
+ */
+
+
+class HookBook {
+
+    function __construct() {
+
+    }
+
+}
+
+new HookBook();
